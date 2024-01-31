aa97320bf84d12c49dfb30d7affe4f4d96fda4b8
seoah29 <seoah29@naver.com>
2024-01-31 17:40:13 +0900

    test

--- src/main/java/com/oz/ozHouse/domain/Category.java
+++ src/main/java/com/oz/ozHouse/domain/Category.java
@@ -14,6 +14,7 @@ public class Category {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int categoryNum;
+	int numnum;
 	private String categoryCode;
 	private String categoryName;
 }
