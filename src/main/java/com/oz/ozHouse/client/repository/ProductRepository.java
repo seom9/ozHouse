32470de3a2bab44db0d3883e0bf43d0c69c5a6fc
LMJ\moonj <moonju1028@naver.com>
2024-02-01 12:53:27 +0900

    main commit

--- src/main/java/com/oz/ozHouse/client/repository/ProductRepository.java
+++ src/main/java/com/oz/ozHouse/client/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.oz.ozHouse.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-	int num;
+	
 }
