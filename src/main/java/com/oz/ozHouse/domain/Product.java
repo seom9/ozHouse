4b08c4a708b54c053f73152c82a39101bc8f50d3
DESKTOP-5BVVMAS\dmssi <dmssid78@hanmail.net>
2024-02-02 20:59:35 +0900

    02/02 21:00 entity 작업 중

--- src/main/java/com/oz/ozHouse/domain/Product.java
+++ src/main/java/com/oz/ozHouse/domain/Product.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.Getter;
@@ -20,28 +22,47 @@ public class Product {
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int proNum;
+	
 	private String proName;
-	private int cateNum;
+	
+	@ManyToMany
+	private Category cateNum;
+	
 	private int merNum;
+	
 	private String proImg;
+	
 	private String proImgPro;
+	
 	private int proQuantity;
+	
 	private int proPrice;
+	
 	private String proModifier;
+	
 	private int proPoint;
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern = "yy/MM/dd")
-	private String proInDate;
+	
 	private String proSpec;
+	
 	private int proPurchasesCount;
+	
 	private String proApprovalStatus;
+	
 	private int proAssemblyCost;
+	
 	private int proDiscountRate;
+	
 	private int proDiscountPrice;
+	
 	private String cateName;
+	
 	private String proImageChange;
+	
 	private String proImageProChange;
+	
 	private String encodedImage;
+	
 	private String inbrandCompany;
+	
 	private String proToday;
 }
