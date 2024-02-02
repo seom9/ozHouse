e23a756ca151a49951c3337b8a0bbce2df96a763
seoah29 <seoah29@naver.com>
2024-02-02 14:11:06 +0900

    Merge branch 'main' of https://github.com/gahyunseoul/ozHouse.git into seoah

--- src/main/java/com/oz/ozHouse/client/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oz.ozHouse.client.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oz.ozHouse.domain.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
