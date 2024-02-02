0f659b83f90ac8d3ac105fba4ecfe355102d09f4
seoah29 <seoah29@naver.com>
2024-02-02 17:40:31 +0900

    판매자 센터 연결

--- src/main/java/com/oz/ozHouse/merchant/controller/MerchantController.java
+++ src/main/java/com/oz/ozHouse/merchant/controller/MerchantController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MerchantController {
-	@GetMapping("merchant_main.do")
+	@GetMapping("merchant-main.do")
 	public String merchantMain() {
 		return "merchant/main/main";
 	}
