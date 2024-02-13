917806a3f92c677ff2e6c2b7d820e5ea1d055b18
sweetPototo <sweetpototo@gmail.com>
2024-02-13 11:35:27 -0800

    complete @PatchMapping

--- src/main/java/com/oz/ozHouse/merchant/controller/MerLoginController.java
+++ src/main/java/com/oz/ozHouse/merchant/controller/MerLoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,19 +35,18 @@ public class MerLoginController {
 	public String login() {
 		return "merchant/join/merchant_login";
 	}
-	
+
 	@PostMapping("/login/loginOk")
-	public String loginOk(HttpServletRequest req, HttpServletResponse resp, 
-			@ModelAttribute MerchantLoginBean loginOk, 
-			@RequestParam(name = "saveId", required=false) String saveId) {
+	public String loginOk(HttpServletRequest req, HttpServletResponse resp, @ModelAttribute MerchantLoginBean loginOk,
+			@RequestParam(name = "saveId", required = false) String saveId) {
 		int res = loginOk.loginOk(loginService, passwordEncoder);
 		String msg = null, url = null;
-		switch(res){
-		case MerchantLoginBean.OK :
+		switch (res) {
+		case MerchantLoginBean.OK:
 			Cookie ck = new Cookie("saveId", loginOk.getMerId());
 			if (saveId != null) {
-				ck.setMaxAge(7*24*60*60);
-			}else {
+				ck.setMaxAge(7 * 24 * 60 * 60);
+			} else {
 				ck.setMaxAge(0);
 			}
 			resp.addCookie(ck);
@@ -55,46 +55,47 @@ public class MerLoginController {
 			msg = loginOk.getMerId() + "님, OZ의 집에 오신걸 환영합니다.";
 			url = "/merchant/main";
 			break;
-		case MerchantLoginBean.NOT_ID :
+		case MerchantLoginBean.NOT_ID:
 			msg = "아이디 확인 후 다시 시도해 주세요";
 			url = "/merchant/login";
 			break;
-		case MerchantLoginBean.NOT_PW :
+		case MerchantLoginBean.NOT_PW:
 			msg = "비밀번호 확인 후 다시 시도해 주세요";
 			url = "/merchant/login";
 			break;
-		case MerchantLoginBean.ERROR : 
+		case MerchantLoginBean.ERROR:
 			msg = "DB 접속 오류! 관리자에게 문의해 주세요";
 			url = "/merchant/main";
 			break;
-		case MerchantLoginBean.DELETE_ID : 
+		case MerchantLoginBean.DELETE_ID:
 			msg = "사용 중지된 ID입니다.";
 			url = "/merchant/main";
 			break;
 		}
 		req.setAttribute("msg", msg);
 		req.setAttribute("url", url);
-    	return "message";
+		return "message";
 	}
-	
-	@GetMapping(value="/login/logout")
-    public String logout(HttpServletRequest req) {
-    	HttpSession session = req.getSession();
-    	session.invalidate();
-    	String msg = "로그아웃 되었습니다.";
-    	String url = "/merchant/main";
-    	req.setAttribute("msg", msg);
-    	req.setAttribute("url", url);
-    	return "message";
-    }
+
+	@GetMapping(value = "/login/logout")
+	public String logout(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		session.invalidate();
+		String msg = "로그아웃 되었습니다.";
+		String url = "/merchant/main";
+		req.setAttribute("msg", msg);
+		req.setAttribute("url", url);
+		return "message";
+	}
+
 	@GetMapping("/login/findMerchant")
 	public String findMerchant() {
 		return "merchant/join/merchant_find";
 	}
-	
+
 	@PostMapping(value = "/login/sendEmail")
-	public String sendEmail1(HttpServletRequest req, 
-			@RequestParam(name = "merEmail")String merEmail) throws Exception {
+	public String sendEmail1(HttpServletRequest req, @RequestParam(name = "merEmail") String merEmail)
+			throws Exception {
 		String mer_id = loginService.checkMerchantIdEmail(merEmail);
 		if (mer_id == null) {
 			req.setAttribute("msg", "가입되어 있지 않은 Email입니다");
@@ -107,72 +108,62 @@ public class MerLoginController {
 		req.setAttribute("member_email", merEmail);
 		return "merchant/join/merchant_send_find_email";
 	}
-	
-	@PostMapping(value="/login/find")
-	public String searchMember(HttpServletRequest req, 
-			@RequestParam(name = "merEmail")String mer_email) {
+
+	@PostMapping(value = "/login/find")
+	public String searchMember(HttpServletRequest req, @RequestParam(name = "merEmail") String mer_email) {
 		String memberId = loginService.checkMerchantIdEmail(mer_email);
-		
+
 		String oauthNum = req.getParameter("oauthNum");
 		String userSendNum = req.getParameter("userSendNum");
-		
+
 		if (oauthNum.equals(userSendNum)) {
 			req.setAttribute("msg", "비밀번호를 재설정해 주세요");
 			req.setAttribute("url", "changePass/" + memberId);
-		}else {
+		} else {
 			req.setAttribute("msg", "인증 번호가 다릅니다 : 다시 시도해 주세요");
 			req.setAttribute("url", "find");
 		}
 		return "message";
 	}
-	
+
 	@GetMapping(value = "/login/changePass/{memberId}")
-    public String mypage_updatePasswd(HttpServletRequest req,
-    		@PathVariable("memberId") String memberId) {
-    	String mode = req.getParameter("mode");
-    	
-    	if (mode != null) {
-    		req.setAttribute("member_id", memberId);
-    	}
-        return "merchant/join/merchant_changePass";
-    }
-	
+	public String mypage_updatePasswd(HttpServletRequest req, @PathVariable("memberId") String memberId) {
+		req.setAttribute("member_id", memberId);
+		return "merchant/join/merchant_changePass";
+	}
+
 	private MerchantDTO getMember(HttpServletRequest req) {
-    	HttpSession session = req.getSession();
-    	MerchantLoginBean merchantLoginMember = (MerchantLoginBean)session.getAttribute("merchantLoginMember");
-    	MerchantDTO dto = loginService.merchant_getMember(merchantLoginMember.getMerId());
-    	return dto;
-    }
-	
-	@PatchMapping(value = "/login/putPassword")
-    public String mypage_updatePasswdPro(HttpServletRequest req) {
-    	MerchantDTO dto = new MerchantDTO();
-    	String id = req.getParameter("member_id");
-    	String new_pass = req.getParameter("new_member_passwd");
-    	
-        boolean passwd = false;
-        
-        	dto = getMember(req);
-            String old_pass = req.getParameter("member_passwd");
-        	passwd = passwordEncoder.matches(old_pass, dto.getMerPw());
-    	
-    	if (passwd) {
-    		dto.setMerPw(new_pass);
-        	boolean res = loginService.updatePass(
-        			dto.getMerPw(), dto.getMerId());
-        	if (res) {
-    			req.setAttribute("msg", "비밀번호가 업데이트 되었습니다");
-    			req.setAttribute("url", "merchant_login.do");
-    		}else{
-    			req.setAttribute("msg", "비밀번호 업데이트 실패 : 수정하실 비밀번호를 확인해 주세요");
-    			req.setAttribute("url", "merchant_login.do");
-    		}
-    	}else {
-    		req.setAttribute("msg", "비밀번호 업데이트 실패 : 다시 확인해 주세요");
+		HttpSession session = req.getSession();
+		MerchantLoginBean merchantLoginMember = (MerchantLoginBean) session.getAttribute("merchantLoginMember");
+		MerchantDTO dto = loginService.merchant_getMember(merchantLoginMember.getMerId());
+		return dto;
+	}
+
+	@PatchMapping(value = "/login/putPassword/{member_id}")
+	public String mypage_updatePasswdPro(HttpServletRequest req, @PathVariable("member_id}") String id, @RequestBody String new_member_passwd) {
+		MerchantDTO dto = new MerchantDTO();
+		boolean passwd = false;
+
+		dto = getMember(req);
+		String old_pass = req.getParameter("member_passwd");
+		passwd = passwordEncoder.matches(old_pass, dto.getMerPw());
+
+		if (passwd) {
+			dto.setMerPw(new_member_passwd);
+			boolean res = loginService.updatePass(dto.getMerPw(), dto.getMerId());
+			if (res) {
+				req.setAttribute("msg", "비밀번호가 업데이트 되었습니다");
+				req.setAttribute("url", "merchant_login.do");
+			} else {
+				req.setAttribute("msg", "비밀번호 업데이트 실패 : 수정하실 비밀번호를 확인해 주세요");
+				req.setAttribute("url", "merchant_login.do");
+			}
+		} else {
+			req.setAttribute("msg", "비밀번호 업데이트 실패 : 다시 확인해 주세요");
 			req.setAttribute("url", "merchant_login.do");
-    	}
-    	
-        return "message";
-    }
-	
+		}
+
+		return "message";
+	}
+
 }
