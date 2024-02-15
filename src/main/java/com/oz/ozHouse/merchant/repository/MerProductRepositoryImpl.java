85916fd593853c2db2f23a5436c4a15f306297e3
seoah29 <seoah29@naver.com>
2024-02-15 17:05:30 +0900

    검색 쿼리 수정중

--- src/main/java/com/oz/ozHouse/merchant/repository/MerProductRepositoryImpl.java
+++ src/main/java/com/oz/ozHouse/merchant/repository/MerProductRepositoryImpl.java
@@ -40,7 +40,7 @@ public class MerProductRepositoryImpl implements MerProductRepository{
 	
 	//재고 리스트
 	@Override
-	public List<ProductDTO> stockList() {
+	public List<ProductDTO> stockList(Map<String, Object> params) {
 		List<Product> productList = em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
 	    
 	    //엔티티 리스트를 DTO 리스트로 변환
@@ -57,131 +57,6 @@ public class MerProductRepositoryImpl implements MerProductRepository{
 		return em.createQuery("SELECT COUNT(p) FROM Product p", Long.class)
 	             .getSingleResult();
 	}
-//	@Override
-//	// 재고 리스트 건수 with dynamic conditions using QueryDSL
-//    public Long stockListCount(Map<String, Object> params) {
-//        // JPAQueryFactory 초기화
-//        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-//        QProduct qProduct = QProduct.product;
-//        
-//        // 기본 쿼리 생성
-//        var query = queryFactory
-//                .select(qProduct.count())
-//                .from(qProduct);
-//
-//        // spec 조건 처리
-//        if (params.containsKey("spec") && params.get("spec") instanceof List) {
-//            List<String> specs = (List<String>) params.get("spec");
-//            // spec 조건에 따른 동적 쿼리 추가
-//            specs.forEach(spec -> {
-//                switch (spec) {
-//                    case "todays":
-//                        query.where(qProduct.productToday.ne("0"));
-//                        break;
-//                    case "best":
-//                        query.where(qProduct.productSpec.eq("best"));
-//                        break;
-//                    case "normal":
-//                        query.where(qProduct.productSpec.eq("normal"));
-//                        break;
-//                }
-//            });
-//        }
-//
-//        // stock 조건 처리
-//        if (params.containsKey("stock")) {
-//            String stock = (String) params.get("stock");
-//            switch (stock) {
-//                case "out":
-//                    query.where(qProduct.productQuantity.eq(0));
-//                    break;
-//                case "almost_out":
-//                    query.where(qProduct.productQuantity.between(1, 5));
-//                    break;
-//                case "good":
-//                    query.where(qProduct.productQuantity.gt(5));
-//                    break;
-//            }
-//        }
-//
-//        // search 조건 처리
-//        if (params.containsKey("search") && params.containsKey("searchString")) {
-//            String search = (String) params.get("search");
-//            String searchString = (String) params.get("searchString");
-//            if (!searchString.isEmpty()) {
-//                switch (search) {
-//                    case "all":
-//                        query.where(qProduct.productNum.like("%" + searchString + "%")
-//                                .or(qProduct.productName.like("%" + searchString + "%")));
-//                        break;
-//                    case "product_num":
-//                        query.where(qProduct.productNum.like("%" + searchString + "%"));
-//                        break;
-//                    case "product_name":
-//                        query.where(qProduct.productName.like("%" + searchString + "%"));
-//                        break;
-//                    // category_name 처리는 관련 필드가 Product 엔티티에 있어야 합니다.
-//                    // 예시: query.where(qProduct.category.name.like("%" + searchString + "%"));
-//                }
-//            }
-//        }
-//
-//        return query.fetchOne();
-//    }
-//	@Override
-//    public Long stockListCount(Map<String, Object> params) {
-//        QProduct qProduct = QProduct.product;
-//
-//        var query = queryFactory.selectFrom(qProduct);
-//
-//        // Dynamic conditions
-//        if (params.containsKey("mer_num")) {
-//            Long merNum = (Long) params.get("mer_num");
-//            query.where(qProduct.merNum.eq(merNum));
-//        }
-//
-//        // Assuming spec is a List<String>
-//        if (params.containsKey("spec")) {
-//            List<String> specs = (List<String>) params.get("spec");
-//            specs.forEach(spec -> {
-//                switch (spec) {
-//                    case "todays":
-//                        query.where(qProduct.productToday.ne("0"));
-//                        break;
-//                    case "best":
-//                        query.where(qProduct.productSpec.eq("best"));
-//                        break;
-//                    case "normal":
-//                        query.where(qProduct.productSpec.eq("normal"));
-//                        break;
-//                }
-//            });
-//        }
-//
-//        if (params.containsKey("stock")) {
-//            String stock = (String) params.get("stock");
-//            switch (stock) {
-//                case "out":
-//                    query.where(qProduct.productQuantity.eq(0));
-//                    break;
-//                case "almost_out":
-//                    query.where(qProduct.productQuantity.between(1, 5));
-//                    break;
-//                case "good":
-//                    query.where(qProduct.productQuantity.gt(5));
-//                    break;
-//            }
-//        }
-//
-//        if (params.containsKey("searchString")) {
-//            String searchString = (String) params.get("searchString");
-//            query.where(qProduct.productNum.like("%" + searchString + "%")
-//                    .or(qProduct.productName.like("%" + searchString + "%")));
-//            // Add additional search conditions as needed
-//        }
-//
-//        return query.fetchCount();
-//    }
 
 	//재고 수정
 
