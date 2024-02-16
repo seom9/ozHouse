4279daabfab54d19912ec48364eda471c31d447f
sweetPototo <sweetpototo@gmail.com>
2024-02-15 18:10:07 -0800

    inbrand Applicaion complete

--- src/main/java/com/oz/ozHouse/merchant/service/MerInbrandService.java
+++ src/main/java/com/oz/ozHouse/merchant/service/MerInbrandService.java
@@ -3,6 +3,7 @@ package com.oz.ozHouse.merchant.service;
 import java.util.Map;
 
 import com.oz.ozHouse.dto.InbrandDTO;
+import com.oz.ozHouse.merchant.Exception.NotFoundMerNumException;
 
 public interface MerInbrandService {
 
@@ -10,5 +11,5 @@ public interface MerInbrandService {
 	public InbrandDTO selectMer(int num);
 	
 	//판매자 번호로 사업자번호 조회
-	public boolean searchComNum(int merNum, Map<String, String> map);
+	public boolean searchComNum(int merNum, Map<String, String> map) throws NotFoundMerNumException;
 }
--- src/main/java/com/oz/ozHouse/merchant/service/MerInbrandServiceImpl.java
+++ src/main/java/com/oz/ozHouse/merchant/service/MerInbrandServiceImpl.java
@@ -9,6 +9,7 @@ import com.oz.ozHouse.domain.Inbrand;
 import com.oz.ozHouse.domain.Merchant;
 import com.oz.ozHouse.dto.InbrandDTO;
 import com.oz.ozHouse.dto.MerchantDTO;
+import com.oz.ozHouse.merchant.Exception.NotFoundMerNumException;
 import com.oz.ozHouse.merchant.repository.MerInbrandRepository;
 import com.oz.ozHouse.merchant.repository.joinRepository.MerJoinRepository;
 
@@ -36,23 +37,22 @@ public class MerInbrandServiceImpl implements MerInbrandService {
 	}
 
 	@Override
-	public boolean searchComNum(int merNum, Map<String, String> map) {
+	public boolean searchComNum(int merNum, Map<String, String> map) throws NotFoundMerNumException {
 		Optional<Merchant> optionalMerchant = joinRepository.findById(merNum);
 		if(optionalMerchant.isPresent()) {
 			Merchant merchant = optionalMerchant.get();
 			String num1 = merchant.getMerComnum().merComnum1;
 			String num2 = merchant.getMerComnum().merComnum2;
 			String num3 = merchant.getMerComnum().merComnum3;
-			if(num1.equals(map.get("inComNum1"))||num2.equals(map.get("inComNum2"))
-					||num3.equals(map.get("inComNum3"))) {
+			if(num1.equals(map.get("inComNum1"))&&num2.equals(map.get("inComNum2"))
+					&&num3.equals(map.get("inComNum3"))) {
 				return true;
 			}else {
 				return false;
 			}
 		}else {
-			dto = null;
+			throw new NotFoundMerNumException("등록된 판매자 번호가 존재하지 않습니다.");
 		}
-		return dto;
 	}
 	
 }
