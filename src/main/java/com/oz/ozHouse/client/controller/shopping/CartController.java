7a6008140a5ed13bb6ca5d02633927a5c06d17d5
gahyunseoul <dmssid78@hanmail.net>
2024-02-21 22:11:24 +0900

    cart

---
+++ src/main/java/com/oz/ozHouse/client/controller/shopping/CartController.java
@@ -0,0 +1,102 @@
+package com.oz.ozHouse.client.controller.shopping;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.oz.ozHouse.client.security.MemberSecurityDTO;
+import com.oz.ozHouse.client.service.CartService;
+import com.oz.ozHouse.dto.ProductDTO;
+import com.oz.ozHouse.dto.client.member.CartDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@Validated
+@RequiredArgsConstructor
+public class CartController {
+	
+	private final CartService cartService;
+	
+	@GetMapping("/cart")
+	public String cartlist_main() {
+		return "client/main/CartList_main";
+	}
+	
+	// Cart : session 저장
+	@GetMapping("/cart/{memberId}/{productNum}/{quantity}")
+	@ResponseBody
+	public String cartAdd (HttpServletRequest req, HttpSession session,
+											@SessionAttribute("cart") List<CartDTO> cart,
+											@AuthenticationPrincipal MemberSecurityDTO member,
+											@PathVariable("memberId") String memberId,
+											@PathVariable("productNum") Integer productNum,
+											@PathVariable("quantity") int quantity)
+											throws IOException {
+		
+		ProductDTO dto = cartService.getProduct(productNum);
+		
+		if (cart == null) cart = new ArrayList<CartDTO>();
+		
+		for (CartDTO cartDTO : cart) {
+			if (cartDTO.getProductDTO().getProNum() == dto.getProNum()) {
+				CartDTO plusCart = CartDTO.builder()
+											.productDTO(dto)
+											.quantity(cartDTO.getQuantity() + quantity)
+											.build();
+				cart.remove(cartDTO);
+				cart.add(plusCart);
+				session.setAttribute("cart", cart);
+				return "redirect:CartList_main.do";
+			}
+		}
+		
+		CartDTO newCart = CartDTO.builder().productDTO(dto).quantity(quantity).build();
+		cart.add(newCart);
+		session.setAttribute("cart", cart);
+		// session.setAttribute("encodedImages", encodedImages);
+		return "redirect:CartList_main.do";
+	}
+	
+	
+	@GetMapping("/cart/{memberId}/{productNum}/{quantity}/del")
+	@ResponseBody
+	public String cartDel (HttpServletRequest req, HttpSession session,
+											@SessionAttribute("cart") List<CartDTO> cart,
+											@AuthenticationPrincipal MemberSecurityDTO member,
+											@PathVariable("memberId") String memberId,
+											@PathVariable("productNum") Integer productNum,
+											@PathVariable("quantity") int quantity)
+											throws IOException {
+		ProductDTO dto = cartService.getProduct(productNum);
+		
+		if (cart == null) cart = new ArrayList<CartDTO>();
+		
+		for (CartDTO cartDTO : cart) {
+			if (cartDTO.getProductDTO().getProNum() == dto.getProNum()) {
+				CartDTO plusCart = CartDTO.builder()
+											.productDTO(dto)
+											.quantity(cartDTO.getQuantity() - quantity)
+											.build();
+				cart.remove(cartDTO);
+				if (plusCart.getQuantity() > 0) cart.add(plusCart);
+				session.setAttribute("cart", cart);
+				return "del";
+			}
+		}
+		
+		return "none";
+	}
+	
+}
