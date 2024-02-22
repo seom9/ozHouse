c066c517418b7214bffa3b21a2759075139240fd
LMJ\moonj <moonju1028@naver.com>
2024-02-22 16:07:31 +0900

    상품 상세 수정

--- src/main/java/com/oz/ozHouse/client/repository/ProductRepository.java
+++ src/main/java/com/oz/ozHouse/client/repository/ProductRepository.java
@@ -3,12 +3,12 @@ package com.oz.ozHouse.client.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.oz.ozHouse.domain.Product;
 import com.oz.ozHouse.dto.ProductDTO;
 
-public interface ProductRepository extends Repository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
 	// 전체 상품 목록
 	List<Product> findAll();
