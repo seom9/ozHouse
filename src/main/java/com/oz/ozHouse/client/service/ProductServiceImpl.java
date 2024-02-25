9ee7782e8577238837e9a23276dcfabefdb9ba6c
LMJ\moonj <moonju1028@naver.com>
2024-02-25 15:17:50 +0900

    Merge branch 'main' of https://github.com/gahyunseoul/ozHouse.git into main

--- src/main/java/com/oz/ozHouse/client/service/ProductServiceImpl.java
+++ src/main/java/com/oz/ozHouse/client/service/ProductServiceImpl.java
@@ -45,15 +45,6 @@ public class ProductServiceImpl implements ProductService{
 		return proList;
 	}
 	
-<<<<<<< HEAD
-   // 상품 상세보기
-   @Override
-   public ProductDTO getProduct(Integer proNum) {
-       Product product = productRepository.findByProNum(proNum);
-       
-       return new ProductDTO(product);
-   }
-=======
 	// 상품 상세보기
 	@Override
 	public ProductDTO getProduct(Integer proNum) {
@@ -62,6 +53,4 @@ public class ProductServiceImpl implements ProductService{
 	    
 	    return new ProductDTO(product);
 	}
->>>>>>> refs/remotes/main/moon
-
 }
