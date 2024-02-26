c305a674e3f62218912d21a0dcc9d605c992eba5
sweetPototo <sweetpototo@gmail.com>
2024-02-26 13:35:49 +0900

    before merge

--- src/main/java/com/oz/ozHouse/merchant/controller/MerReturnController.java
+++ src/main/java/com/oz/ozHouse/merchant/controller/MerReturnController.java
@@ -3,6 +3,7 @@ package com.oz.ozHouse.merchant.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,17 +13,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/merchant/{merNum}/store")
 @RequiredArgsConstructor
 public class MerReturnController {
-	private final MerReturnService reService;
+//	private final MerReturnService reService;
 	
 	@GetMapping("/returns")
 	public String returnCancelList(HttpServletRequest req) {
-		List<ListDTO> list = reService.returnCancleList(map);
-		int returnCount = reService.countReturn(map);
-		req.setAttribute("searchOptions", settinOption("all"));
-		req.setAttribute("radioOptions", settinRadio("all"));
-		req.setAttribute("orderReturnList", list);
-		req.setAttribute("returnCount", returnCount);
-		req.setAttribute("map", map);
+//		List<ListDTO> list = reService.returnCancleList(map);
+//		int returnCount = reService.countReturn(map);
+//		req.setAttribute("searchOptions", settinOption("all"));
+//		req.setAttribute("radioOptions", settinRadio("all"));
+//		req.setAttribute("orderReturnList", list);
+//		req.setAttribute("returnCount", returnCount);
+//		req.setAttribute("map", map);
 		return "merchant/store/returnCancle/returnCancle_returnList";
 	}
 }
